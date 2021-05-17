a491c2454b7fa4ec70728d16ce0ea023b51fa487
gokul1630 <gokulgp163034@gmail.com>
2021-05-17 17:29:57 +0530

    Return json response
    use single function to get all json response & return json response instead of message

--- app.py
+++ app.py
@@ -8,6 +8,18 @@ connection = sqlite3.connect("log.db", check_same_thread=False)
 connection.row_factory = sqlite3.Row
 cursor = connection.cursor()
 
+
+def getAllResponse():
+    result = cursor.execute("SELECT * FROM people").fetchall()
+    jsonResponse = {"users": [dict(i) for i in result]}
+    response = app.response_class(
+        response=json.dumps(jsonResponse),
+        status=200,
+        mimetype='application/json'
+    )
+    return response
+
+
 try:
     cursor.execute(
         "CREATE TABLE IF NOT EXISTS people (name TEXT,age INT,id INT)")
@@ -18,7 +30,8 @@ except Exception as e:
 
 @app.route('/', methods=['GET'])
 def root():
-    response = {"message": "Welcome To Flask Server", "status": "Flask server running"}
+    response = {"message": "Welcome To Flask Server",
+                "status": "Flask server running"}
     return json.dumps(response)
 
 
@@ -31,7 +44,7 @@ def put_db():
     if user is not None and age is not None and userId is not None:
         cursor.execute(f"INSERT INTO people VALUES (?,?,?)", params)
         connection.commit()
-        return f'Data added Successfully for user: {user}'
+        return getAllResponse()
     else:
         return "please submit all values"
 
@@ -41,25 +54,27 @@ def get_db():
     user = request.args.get('user')
     Id = request.args.get('id')
     if user is not None:
-        res = cursor.execute('select * from people where name=?', (user,)).fetchall()
+        res = cursor.execute(
+            'select * from people where name=?', (user,)).fetchall()
         if len(res) == 0:
             return f"User {user} not found on database"
         else:
-            result = cursor.execute("SELECT * FROM people WHERE name=?", (user,)).fetchall()
+            result = cursor.execute(
+                "SELECT * FROM people WHERE name=?", (user,)).fetchall()
             response = {"users": [dict(i) for i in result]}
             return json.dumps(response)
     if Id is not None:
-        res = cursor.execute('select * from people where id=?', (Id,)).fetchall()
+        res = cursor.execute(
+            'select * from people where id=?', (Id,)).fetchall()
         if len(res) == 0:
             return f"Id {Id} not found on database"
         else:
-            result = cursor.execute("SELECT * FROM people WHERE id=?", (Id,)).fetchall()
+            result = cursor.execute(
+                "SELECT * FROM people WHERE id=?", (Id,)).fetchall()
             response = {"users": [dict(i) for i in result]}
             return json.dumps(response)
     else:
-        result = cursor.execute("SELECT * FROM people").fetchall()
-        response = {"users": [dict(i) for i in result]}
-        return json.dumps(response)
+        getAllResponse()
 
 
 @app.route('/update', methods=['PATCH'])
@@ -68,21 +83,23 @@ def update_db():
     age = request.args.get('age')
     Id = request.args.get('id')
     if user and Id is not None:
-        res = cursor.execute('select * from people where id=?', (Id,)).fetchall()
+        res = cursor.execute(
+            'select * from people where id=?', (Id,)).fetchall()
         if len(res) == 0:
             return f"Id {Id} doesn't exists on database"
         else:
             cursor.execute('UPDATE people SET name=? WHERE id=?', (user, Id,))
             connection.commit()
-        return "User updated Successfully"
+            return getAllResponse()
     if age and Id is not None:
-        res = cursor.execute('select * from people where id=?', (Id,)).fetchall()
+        res = cursor.execute(
+            'select * from people where id=?', (Id,)).fetchall()
         if len(res) == 0:
             return f"Id {Id} doesn't exists on database"
         else:
             cursor.execute('UPDATE people SET age=? WHERE id=?', (age, Id,))
             connection.commit()
-        return "User updated Successfully"
+            return getAllResponse()
     else:
         return "Provide value for update database"
 
@@ -92,21 +109,23 @@ def delete_db():
     Id = request.args.get('id')
     user = request.args.get('user')
     if Id is not None:
-        res = cursor.execute('select * from people where id=?', (Id,)).fetchall()
+        res = cursor.execute(
+            'select * from people where id=?', (Id,)).fetchall()
         if len(res) == 0:
             return f"Id {Id} doesn't exists on database"
         else:
             cursor.execute('DELETE FROM people WHERE id=?', (Id,))
             connection.commit()
-        return f"User {Id} deleted Successfully"
+            return getAllResponse()
     elif user is not None:
-        res = cursor.execute('select * from people where name=?', (user,)).fetchall()
+        res = cursor.execute(
+            'select * from people where name=?', (user,)).fetchall()
         if len(res) == 0:
             return f"User {user} doesn't exists on database"
         else:
             cursor.execute('DELETE FROM people WHERE name=?', (user,))
             connection.commit()
-        return f"User {user} deleted Successfully"
+            return getAllResponse()
     else:
         return "please provide values to delete user"
 
